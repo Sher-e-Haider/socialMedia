8e9ef8bd21c71934cfa3c7d1f633fd1450a23949
md sahil raza <razasahil263@gmail.com>
2022-05-11 21:25:56 +0530

    changes

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 
-const API = Axios.create({ baseURL: 'http://localhost:5000' });
+const API = Axios.create({ baseURL: 'https://social-sahil.herokuapp.com' });
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,7 +1,7 @@
 import  Axios  from "axios";
 import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "./constant";
 
-const url = 'http://localhost:5000/auth'
+const url = 'https://social-sahil.herokuapp.com/auth'
 export const signin = (email,password,history)=> async (dispatch)=>{
     dispatch({type:USER_SIGNIN_REQUEST,payload:{email,password}})
     try {
